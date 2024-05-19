129792dfd88d03c05dc36ad83fc39746e45dd623
Ding-Yang Liu <199928ltyos@gmail.com>
2024-05-20 02:34:20 +0800

    Use config.sh to reduce code duplication.

---
+++ config.sh
@@ -0,0 +1,5 @@
+project_dir="/home/blue/code/TCGA_CHOL_2sample"
+sample_sheet="${project_dir}/gdc_sample_sheet_test.2024-02-26.tsv"
+GDC_token="${project_dir}/gdc-user-token.2024-05-15T09_11_50.917Z.txt"
+tools_dir="${project_dir}/bioinformatics-tools"
+threads=12

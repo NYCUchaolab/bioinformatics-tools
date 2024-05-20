aeffd0b4baccdaa2de695ad1ae63cba2a68a5c15
Ding-Yang Liu <199928ltyos@gmail.com>
2024-05-20 20:19:08 +0800

    add bash Shebang

--- WES_shell/norm_vcf.sh
+++ WES_shell/norm_vcf.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 input_vcf="/home/data/dataset/CHOL_10sample/variants_calling/mutect/TCGA-4G-AAZF.vcf"
 output_vcf="/home/data/dataset/CHOL_10sample/variants_calling/mutect/TCGA-4G-AAZF_splited.vcf"
 ref_fa="/home/data/dataset/BWA_index/GRCh38.d1.vd1.fa"

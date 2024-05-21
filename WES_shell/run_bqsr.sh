a4a6a3c27295af3ec8350faf24664a543f2d1329
Ding-Yang Liu <199928ltyos@gmail.com>
2024-05-21 16:51:15 +0800

    update BQSR code

--- WES_shell/run_bqsr.sh
+++ WES_shell/run_bqsr.sh
@@ -1,79 +1,59 @@
 #!/bin/bash
 
-
 bqsr(){
     # input: re.bam
     # intermiade: .re.sorted.bam, re.sorted.du.bam, re.sorted.du.matrix.txt, recalibration_report.txt
     # output: re.sorted.du.bqsr.bam
-
     sample_ID=${1}
 
     # step1. bam file排序
     java -Xmx50g -jar ${picard} SortSam \
 		I="${input_bam_dir}/${sample_ID}.bam" \
-		O="${output_bam_dir}/${sample_ID}.re.sorted.bam" \
+		O="${output_bam_dir}/${sample_ID}.sorted.bam" \
 		SO=coordinate \
         VALIDATION_STRINGENCY=STRICT \
         CREATE_INDEX=true
 
     # step2. 將重複序列進行標注
 	java -jar ${picard} MarkDuplicates \
-		I="${output_bam_dir}/${sample_ID}.re.sorted.bam" \
-		O="${output_bam_dir}/${sample_ID}.re.sorted.du.bam" \
-		M="${output_bam_dir}/${sample_ID}.re.sorted.du.matrix.txt" \
+		I="${output_bam_dir}/${sample_ID}.sorted.bam" \
+		O="${output_bam_dir}/${sample_ID}.sorted.du.bam" \
+		M="${output_bam_dir}/${sample_ID}.sorted.du.matrix.txt" \
 		REMOVE_DUPLICATES=false \
         CREATE_INDEX=true 2>> MARK_DUP.log
 
     # step3. 校正base品質
 	gatk BaseRecalibrator \
 		-R ${ref_fa} \
-		-I "${output_bam_dir}/${sample_ID}.re.sorted.du.bam" \
+		-I "${output_bam_dir}/${sample_ID}.sorted.du.bam" \
 		-O "${output_bam_dir}/${sample_ID}.recalibration_report.txt" \
 		--known-sites ${dbsnp}
 
     # step4. 套用校正
     gatk ApplyBQSR \
 		-R ${ref_fa} \
-		-I "${output_bam_dir}/${sample_ID}.re.sorted.du.bam" \
-		-O "${output_bam_dir}/${sample_ID}.re.sorted.du.bqsr.bam" \
+		-I "${output_bam_dir}/${sample_ID}.sorted.du.bam" \
+		-O "${output_bam_dir}/${sample_ID}.sorted.du.bqsr.bam" \
 		--bqsr-recal-file "${output_bam_dir}/${sample_ID}.recalibration_report.txt"
 }
 
 # config 
-env_name="wes" # 環境名稱請確認
-source "/home/${USER}/miniconda3/etc/profile.d/conda.sh" 
-conda activate ${env_name}
-threads=20
-picard="/home/${USER}/miniconda3/envs/${env_name}/share/picard-*/picard.jar" # 抓取使用者的環境資料夾
-
-
-# ref config
-db_dir="/home/data/dataset/gatk_file"
-dbsnp="${db_dir}/Homo_sapiens_assembly38.dbsnp138.vcf.gz"
-ref_fa="/home/data/dataset/BWA_index/GRCh38.d1.vd1.fa"
+source "../config.sh"
+conda activate wes-preprocessing
+picard="/home/${USER}/miniconda3/envs/wes-preprocessing/share/picard-*/picard.jar" # 抓取使用者的環境資料夾
 
 # project config
-project_dir="/home/data/dataset/CHOL_10sample"
 cd ${project_dir}
-samplesheet_path="${project_dir}/gdc_sample_sheet_test.2024-02-26.tsv"
+input_bam_dir="${project_dir}/bam"
+output_bam_dir="${project_dir}/bqsr_bam"
+mkdir -p ${output_bam_dir}
+mkdir -p ${output_bam_dir}/log
 
-input_bam_dir="./output_bam_biobambam"
-output_bam_dir="./bqsr_bam"
-mkdir -p ./bqsr_bam
-mkdir -p ./bqsr_bam/log
-
-# col 6 sample_ID TCGA-4G-AAZR sort 並去除重複
-sample_IDs=($(tail -n +2 ${samplesheet_path} | cut -f 6 | sort -u))
-
-# for sample sample_IDs
-
-for sample_ID in "${sample_IDs[@]}"
-do
-
-    echo "${sample_ID}"
-	tumor="${sample_ID}-01A"
-    normal="${sample_ID}-11A"
-	
-    bqsr ${tumor} >> "./bqsr_bam/log/${tumor}" 2>&1
-    bqsr ${normal} >> "./bqsr_bam/log/${normal}" 2>&1
-done
+case_IDs_file="${project_dir}/case_IDs.tsv"
+while IFS= read -r case_ID
+	do
+		echo "Processing ${case_ID}"
+		bqsr "${case_ID}-T" > "${output_bam_dir}/log/${case_ID}-T.bqsr.log" 2>&1 || { echo "Error processing ${case_ID}-T"; exit 1; }
+		bqsr "${case_ID}-N" > "${output_bam_dir}/log/${case_ID}-N.bqsr.log" 2>&1 || { echo "Error processing ${case_ID}-N"; exit 1; }
+		python "${tools_dir}/others/send_msg.py" "BQSR ${case_IDs} done"
+	done < "$case_IDs_file"

b50d0cc7313805cbaa44174f5ca7f8f47f40f33b
Ding-Yang Liu <199928ltyos@gmail.com>
2024-06-21 09:56:38 +0800

    update cnvkit + gistic (error)

--- WES_shell/run_cnvkit.sh
+++ WES_shell/run_cnvkit.sh
@@ -1,34 +1,66 @@
 #!/bin/bash
 
+run_cnvkit(){
+        bam_input_dir=${1}
+        cnv_output_dir=${2}
+        # call copy number variants
+        ${cnvkit} batch ${project_dir}/${bam_input_dir}/*T*.bam \
+                --normal ${project_dir}/${bam_input_dir}/*B*.bam \
+                --targets ${illumina_bed} --annotate ${refFlat} \
+                --fasta ${ref_fa} --access ${access} -p ${threads}\
+                --output-reference ${cnv_output_dir}/cnv_ref.cnn --output-dir ${cnv_output_dir} \
+                --diagram --scatter  > ${cnv_output_dir}/log/cnvkit.log 2>&1
+        # export to gistic format
+        ${cnvkit} export seg $cnv_output_dir/*.cns \
+                -o ${cnv_output_dir}/gistic.segments >> ${cnv_output_dir}/log/cnvkit.log 2>&1
+}
+
+run_gistic2(){
+        gistic_output_dir={$1}
+        # run gistic
+        $gistic -b ${gistic_output_dir} \
+                -seg ${cnv_output_dir}/gistic.segments\
+                -refgene ${gistic_ref} \
+                -genegistic 1 \
+                -smallmem 1 \
+                -broad 1 \
+                -brlen 0.5 \
+                -conf 0.90 -armpeel 1 -savegene 1 -gcm "extreme"
+}
+
 # conda 
-env_name="wes" # 環境名稱請確認
+env_name="wes-cnv" # 環境名稱請確認
 source "/home/${USER}/miniconda3/etc/profile.d/conda.sh" 
 conda activate ${env_name}
-# cnvkit="$HOME/miniconda3/envs/wes/bin/cnvkit.py"
+cnvkit="$HOME/miniconda3/envs/${env_name}/bin/cnvkit.py"
 
 # ref
 ref_fa="/home/data/dataset/BWA_index/GRCh38.d1.vd1.fa" # 修改ref位置
+gistic_dir="/home/data/data_dingyangliu/ntu-tki/GISTIC2"
+gistic_ref="${gistic_dir}/refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat"
+gistic="${gistic_dir}/gistic2"
+#export PATH=${gistic_dir}:$PATH
+export MCR_ROOT=/home/data/data_dingyangliu/ntu-tki/GISTIC2/MATLAB_Compiler_Runtime
+export LD_LIBRARY_PATH=$MCR_ROOT/v92/runtime/glnxa64:$MCR_ROOT/v92/bin/glnxa64:$MCR_ROOT/v92/sys/os/glnxa64:$LD_LIBRARY_PATH
 
 # config 
-threads=20
-project_dir="/home/data/dataset/CHOL_10sample" # 專案資料夾
+project_dir="/home/data/data_dingyangliu/ntu-tki" # 專案資料夾
 cd ${project_dir}
-access="./cnv_ref/access-10kb.hg38.bed"
-refFlat="./cnv_ref/refFlat.txt"
-#illumina_bed="./cnv_ref/Illumina_Exome_TargetedRegions_v1.2.hg38.bed"
-illumina_bed="./cnv_ref/Twist_ILMN_Exome_2.0_Plus_Panel.hg38.bed"
-input_bam_dir="sorted_du_bqsr_bam_biobambam"
-cnv_output_dir="./variants_calling/cnv"
+access="access-10kb.hg38.bed"
+refFlat="refFlat_hg38.txt"
+illumina_bed="hg38_Twist_ILMN_Exome_2.5_Panel_Combined_Mito.bed"
+
+bam_input_dir="bam"
+cnv_output_dir="./variants_calling/cnv_TB"
+gistic_output_dir="./variants_calling/gistic_test_TB"
 
 # mkdir
 mkdir -p variants_calling
 mkdir -p ${cnv_output_dir}
 mkdir -p ${cnv_output_dir}/log
+mkdir -p ${gistic_output_dir}
 cd ${project_dir}
 
-cnvkit.py batch ${project_dir}/${input_bam_dir}/*01A*.bam \
-        --normal ${project_dir}/${input_bam_dir}/*11A*.bam \
-        --targets ${illumina_bed} --annotate ${refFlat} \
-        --fasta ${ref_fa} --access ${access} -p ${threads}\
-        --output-reference ${cnv_output_dir}/cnv_ref.cnn --output-dir ${cnv_output_dir} \
-        --diagram --scatter  > ${cnv_output_dir}/log/cnvkit.log 2>&1
+# call functions 
+# run_cnvkit ${bam_input_dir} ${cnv_output_dir}
+run_gistic2 ${gistic_output_dir}

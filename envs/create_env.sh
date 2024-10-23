83f38be17b048d92969cb9f4daa7bbc7518c838b
Blue <44969795+xu62u4u6@users.noreply.github.com>
2024-10-23 17:52:34 +0800

    Update variants calling packages version

--- envs/create_env.sh
+++ envs/create_env.sh
@@ -15,8 +15,8 @@ conda install mamba -y
 mamba install fastqc multiqc gatk4 samtools bedtools bwa picard qualimap trimmomatic pandas requests -y
 
 # variants calling
-conda create -n wes-variants-calling python=3.8 -y
+conda create -n wes-variants-calling python=3.6 -y
 conda activate wes-variants-calling
 conda install mamba -y
-mamba install gatk4 pindel muse varscan bcftools openjdk=8 samtools ensembl-vep=103.1 -y
+mamba install gatk4=4.1 pindel muse varscan bcftools openjdk=8 samtools ensembl-vep=103.1 -y
 mamba install pandas matplotlib perl-compress-raw-zlib=2.202 -y
